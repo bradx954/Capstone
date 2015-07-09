cff603c108f4842567baf86dc25896797de2c4fc
brad <brad.baago@linux.com>
2015-07-09 17:23:37 -0400

    Code clean up.

--- Application/Views/home.php
+++ Application/Views/home.php
@@ -10,4 +10,5 @@
 </center>
 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc hendrerit, nisl in lobortis ullamcorper, sapien sem suscipit nisl, eu pulvinar eros lectus at elit. Mauris at pharetra massa. Vestibulum nec faucibus ante, et egestas purus. Curabitur suscipit dictum feugiat. Morbi id vehicula dui, sed iaculis lorem. Vivamus eu fringilla massa. Sed ultrices, odio porttitor auctor auctor, lacus velit scelerisque libero, eu laoreet ex mauris quis enim. Sed massa turpis, maximus ac sodales in, facilisis et nisi. Suspendisse efficitur metus at nisl egestas sollicitudin. Vestibulum quis lacus aliquet, tincidunt urna quis, tristique turpis.</p>
 <div>
-<?php require_once 'Application/Views/Snippets/CredentialWindows.php';?>
+<?php require_once 'Application/Views/Snippets/SignUp.php';?>
+<?php require_once 'Application/Views/Snippets/Login.php';?>
