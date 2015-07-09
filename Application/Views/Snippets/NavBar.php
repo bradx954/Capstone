4aa69775efe400b9141bc634fdcc0416a6299f36
brad <brad.baago@linux.com>
2015-07-09 19:58:04 -0400

    Tweak.

--- Application/Views/Snippets/NavBar.php
+++ Application/Views/Snippets/NavBar.php
@@ -9,27 +9,14 @@
 			</button>
 			<a class='navbar-brand' href='index.php'>Storacloud</a>
 		</div>
-        <div class='nav navbar-nav' id='standard'>
+        <div class='nav navbar-nav'>
             <li class='active'><a href='index.php?c=home'>Home</a></li>
             <li><a href='#'>Files</a></li>
         </div>
-        <div class='nav navbar-nav navbar-right' id='standard'>
+        <div class='nav navbar-nav navbar-right'>
 			<li><a><?php echo $_SESSION['auth']['email']; ?></a></li>
             <li><a href='#'>Settings</a></li>
             <li><a href='index.php?c=home&m=logout'>Logout</a></li>
-		</div>
-		<div class='nav navbar-nav navbar-right' id='mobile'>
-			<ul class='nav navbar-nav'>
-			<li class='dropdown' id='dropdown'>
-				<a href='#' class='dropdown-toggle' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'><?php echo $_SESSION['auth']['email']; ?></a>
-				<ul class='dropdown-menu'>
-                <li class='active'><a href='index.php?c=home'>Home</a></li>
-				<li><a href='#'>Files</a></li>
-				<li><a href='#'>Settings</a></li>
-				<li><a href='index.php?c=home&m=logout'>Logout</a></li>
-				</ul>
-			</li>
-			</ul>
 		</div>
 	</div>
 </nav>
