7c8984b8de97b2be9362ed0bfb880e82c8489fae
brad <brad.baago@linux.com>
2015-07-09 19:21:12 -0400

    Minor bug fix.

--- Application/Views/Snippets/NavBar.php
+++ Application/Views/Snippets/NavBar.php
@@ -12,7 +12,7 @@
 		<div class='nav navbar-nav navbar-right' id='bs-example-navbar-collapse-1'>
 			<ul class='nav navbar-nav'>
 			<li class='dropdown'>
-				<a href='#' class='dropdown-toggle' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'><?php echo $_SESSION['auth']['email'] ?></a>
+				<a href='#' class='dropdown-toggle' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'><?php echo $_SESSION['auth']['email']; ?></a>
 				<ul class='dropdown-menu'>
 				<li><a href='#'>Files</a></li>
 				<li><a href='#'>Settings</a></li>
