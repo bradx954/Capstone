7f8ba1262b6f1ad46168fd6286ae00395064ecab
brad <brad.baago@linux.com>
2015-07-10 19:37:07 -0400

    test.

--- Application/Views/admin.php
+++ Application/Views/admin.php
@@ -1,4 +1,4 @@
 <h1>Admin Zone</h1>
 <pre><?php echo print_r($GLOBALS['config']);?></pre>
 <pre><?php echo print_r($_SESSION);?></pre>
-<pre><?php echo print_r($GLOBALS['config']['acl']['admin']);?></pre>
+<pre><?php echo print_r($GLOBALS['config']['acl']['admin']['admin']);?></pre>
