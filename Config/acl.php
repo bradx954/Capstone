91c4f90439325ce2db5867f2d20cf6f1cc9b14c6
brad <brad.baago@linux.com>
2015-07-10 18:54:21 -0400

    Bug FIx.

--- Config/acl.php
+++ Config/acl.php
@@ -3,8 +3,8 @@
 //access control list		
  $config['acl'] = array('home' 			=> 		array('public' => true, 'user' => true,		'admin'=>true,	'superadmin'=>true),
 					'files' 			=> 		array('public' => false, 'user' => true,		'admin'=>true,	'superadmin'=>true),
-					'users' 		=> 		array('public' => false, 'user' => false,		'admin'=>true,	'superadmin'=>true),
-                    'server' 		=> 		array('public' => false, 'user' => false,		'admin'=>false,	'superadmin'=>true)
+					'admin' 		=> 		array('public' => false, 'user' => false,		'admin'=>true,	'superadmin'=>true),
+                    'superadmin' 		=> 		array('public' => false, 'user' => false,		'admin'=>false,	'superadmin'=>true)
  					);
 
 ?>
