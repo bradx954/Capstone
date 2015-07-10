d5961177687efc778ee5293b40ee645ce411c5f1
brad <brad.baago@linux.com>
2015-07-10 17:23:40 -0400

    Permisions Update.

--- Config/acl.php
+++ Config/acl.php
@@ -1,14 +1,10 @@
 <?php
 
 //access control list		
- $config['acl'] = array('home' 			=> 		array('public' => true,		'bronze'=>true,	'silver'=>true,	'gold'=>true, 	'admin' => true),
-					'staff' 			=> 		array('public' => false,	'bronze'=>true,	'silver'=>true,	'gold'=>false, 	'admin' => true),
-					'supervisors' 		=> 		array('public' => false,	'bronze'=>false,'silver'=>true,	'gold'=>false, 	'admin' => true),
-					'helpdesk' 			=> 		array('public' => false,	'bronze'=>false,'silver'=>false,'gold'=>true, 	'admin' => true),
-					'admin' 			=> 		array('public' => false,	'bronze'=>false,'silver'=>false,'gold'=>false, 	'admin' => true),
-					'login' 			=> 		array('public' => true,		'bronze'=>true,	'silver'=>true,	'gold'=>true, 	'admin' => true),
-					'logout' 			=> 		array('public' => true,		'bronze'=>true,	'silver'=>true,	'gold'=>true, 	'admin' => true),
-					'register' 			=> 		array('public' => true,		'bronze'=>true,	'silver'=>true,	'gold'=>true, 	'admin' => true) 
+ $config['acl'] = array('home' 			=> 		array('public' => true, 'user' => true,		'admin'=>true,	'superadmin'=>true),
+					'files' 			=> 		array('public' => false, 'user' => true,		'admin'=>true,	'superadmin'=>true),
+					'users' 		=> 		array('public' => false, 'user' => false,		'admin'=>true,	'superadmin'=>true),
+                    'server' 		=> 		array('public' => false, 'user' => false,		'admin'=>false,	'superadmin'=>true)
  					);
 
 ?>
