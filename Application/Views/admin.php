7e0e730c8eaedbbfcbf4ef8f469c75f4d80d68d0
brad <brad.baago@linux.com>
2015-07-10 19:38:58 -0400

    Bug Fix.

--- Application/Views/admin.php
+++ Application/Views/admin.php
@@ -1,4 +1,4 @@
 <h1>Admin Zone</h1>
 <pre><?php echo print_r($GLOBALS['config']);?></pre>
 <pre><?php echo print_r($_SESSION);?></pre>
-<pre><?php echo print_r($GLOBALS['config']['acl']['admin'][$_SESSION['accesslevel']]);?></pre>
+<pre><?php echo print_r($GLOBALS['config']['acl']['admin'][$_SESSION['auth']['accesslevel']]);?></pre>
