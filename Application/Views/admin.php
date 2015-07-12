181fd0354c3b96c939c72664ef2e8c326bf1ab11
brad <brad.baago@linux.com>
2015-07-12 01:02:20 -0400

    Updated admin view with table.

--- Application/Views/admin.php
+++ Application/Views/admin.php
@@ -1,2 +1,29 @@
-<h1>Admin Zone</h1>
-<pre><?php echo print_r($TPL);?></pre>
+<div class="table-responsive">
+    <table class='table table-striped table-bordered table-hover'>
+    <tr>
+        <th>ID</th>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th>Email</th>
+        <th>Quota</th>
+        <th>Rank</th>
+        <th>Date Registered</th>
+        <th>Enabled</th>
+    </tr>
+    <?php
+        foreach($TPL['users'] as $user)
+        {
+            echo "<tr>";
+                echo "<td>".$user['id']."</td>";
+                echo "<td>".$user['firstname']."</td>";
+                echo "<td>".$user['lastname']."</td>";
+                echo "<td>".$user['email']."</td>";
+                echo "<td>".$user['quota']."</td>";
+                echo "<td>".$user['rank']."</td>";
+                echo "<td>".$user['reg_date']."</td>";
+                echo "<td>".$user['active']."</td>";
+            echo "</tr>";
+        }
+    ?>
+    </table>
+</div>
