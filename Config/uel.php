06fe53d0ca8eb45025c8328db137e37f54f85472
brad <brad.baago@linux.com>
2015-07-21 03:04:29 -0400

    Bug fix.

--- Config/uel.php
+++ Config/uel.php
@@ -1,6 +1,7 @@
-//user edit list
+<?php //user edit list
 $config['uel'] = array(
     'user' => array('public' => false,'user' => false,'admin'=>true,'superadmin'=>true),
     'admin' => array('public' => false,'user' => false,'admin'=>false,'superadmin'=>true),
     'superadmin' => array('public' => false,'user' => false,'admin'=>false,'superadmin'=>false),
 );
+?>
