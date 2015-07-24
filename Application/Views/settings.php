33ec23dcdfd2bcf381a21b8deffc8099ef9dd414
brad <brad.baago@linux.com>
2015-07-23 21:30:18 -0400

    Tweak.

--- Application/Views/settings.php
+++ Application/Views/settings.php
@@ -7,7 +7,7 @@ function openFileImage()
 <div id="avatar">
     <?php echo "<img height='256' width='256' src='data:image/png;base64,".$_SESSION['auth']['avatar']."' />"; ?>
     <form id='avatar-form'>
-        <input type="file" id="avatar" style="display:none">
+        <input type="file" id="avatar">
     <form>
     <button type="button" class="btn btn-primary" onClick='openFileImage();return;'>Change</button>
     <button type="button" class="btn btn-default">Delete</button>
