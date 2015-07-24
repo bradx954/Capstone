20b1e274064f90c62e1efa6870063d4b80b2b171
brad <brad.baago@linux.com>
2015-07-23 21:29:42 -0400

    Update to settings.

--- Application/Views/settings.php
+++ Application/Views/settings.php
@@ -1,5 +1,14 @@
+<script>
+function openFileImage()
+{
+  $("#avatar").click();
+}
+</script>
 <div id="avatar">
-    <?php echo "<img src='data:image/png;base64,".$_SESSION['auth']['avatar']."' />"; ?>
-    <button type="button" class="btn btn-primary">Change</button>
+    <?php echo "<img height='256' width='256' src='data:image/png;base64,".$_SESSION['auth']['avatar']."' />"; ?>
+    <form id='avatar-form'>
+        <input type="file" id="avatar" style="display:none">
+    <form>
+    <button type="button" class="btn btn-primary" onClick='openFileImage();return;'>Change</button>
     <button type="button" class="btn btn-default">Delete</button>
 </div>
