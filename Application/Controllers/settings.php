ad4dd27194b3d4da450fb4e17ff120fcb5c24fcd
brad <brad.baago@linux.com>
2015-07-24 01:44:28 -0400

    Bug fix.

--- Application/Controllers/settings.php
+++ Application/Controllers/settings.php
@@ -47,7 +47,7 @@ class Settings extends Controller
     }
     function updateUserAvatar()
     {
-        $avatar = $_POST['newImage'];
+        $avatar = $_FILES['newImage'];
         return $this->M_Users->updateUserAvatar($this->UserID,$avatar);
     }
 }
