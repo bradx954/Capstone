14889df1a9c647eb1b093d386754cf71c735d523
Brad Baago <bbaago@gmail.com>
2015-08-03 15:15:34 -0400

    Added module framework for user fields.

---
+++ Application/Views/Snippets/UserFieldsEditModule.php
@@ -0,0 +1,4 @@
+<script src='Web/JS/UserFieldsEditModule.js'></script>
+<div id="UserFields" class="module-container" style="width: 300px;">
+    <h2 style="text-align: center; margin: 0px;">Fields</h2>
+</div>
