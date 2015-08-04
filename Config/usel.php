bdf74f45fb39390c74fc202e3d8c87a05350325b
Brad Baago <bbaago@gmail.com>
2015-08-03 21:22:07 -0400

    Added file.

---
+++ Config/usel.php
@@ -0,0 +1,7 @@
+<?php
+    $config['usel'] = array(
+    'email' => array('public' => false,'user' => false,'admin'=>true,'superadmin'=>true),
+    'fname' => array('public' => false,'user' => true,'admin'=>true,'superadmin'=>true),
+    'lname' => array('public' => false,'user' => true,'admin'=>true,'superadmin'=>true),
+);
+?>
