48751dcf04d02b213664e0b20081951cb0dd936e
Brad Baago <bbaago@gmail.com>
2015-08-07 16:55:00 -0400

    Added user details module to settings.

---
+++ Application/Views/Snippets/UserDetailsModule.php
@@ -0,0 +1,4 @@
+<script src='Web/JS/UserDetailsModule.js'></script>
+<div id="UserDetails" class="module-container" style="width: 300px;">
+    <h2 style="text-align: center; margin: 0px;">Details</h2>
+</div>
--- Application/Views/settings.php
+++ Application/Views/settings.php
@@ -1,4 +1,5 @@
 <?php
 	require_once 'Application/Views/Snippets/AvatarEditModule.php';
     require_once 'Application/Views/Snippets/UserFieldsEditModule.php';
+    require_once 'Application/Views/Snippets/UserDetailsModule.php';
 ?>
