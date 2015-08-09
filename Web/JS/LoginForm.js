8869f5b0cbe855743383df019ab8cd49c08d5f07
Brad Baago <bbaago@gmail.com>
2015-08-09 15:39:47 -0400

    More work on forgot password feature.

--- Web/JS/LoginForm.js
+++ Web/JS/LoginForm.js
@@ -27,4 +27,27 @@ $(document).ready(function () {
         $("#LoginFormHome").css('display', 'none');
         $("#LoginFormEmail").css('display', 'block');
     });
+    $("#SubmitLoginFormEmail").click(function () {
+        $("#LoginFormEmail").submit(function (e) {
+            var postData = $(this).serializeArray();
+            var formURL = $(this).attr("action");
+            $.ajax(
+            {
+                url: formURL,
+                type: "POST",
+                data: postData,
+                success: function (data, textStatus, jqXHR) {
+                    if (data == "Login Success") { window.location.assign("index.php"); }
+                    else { document.getElementById('LoginFormMessage').innerHTML = '<div class="alert alert-danger">' + data + '</div>'; }
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    document.getElementById('LoginFormMessage').innerHTML = '<div class="alert alert-danger">' + errorThrown + '</div>';
+                }
+            });
+            e.preventDefault();	//STOP default action
+            //e.unbind();
+            $("#LoginFormEmail").unbind('submit');
+        });
+        $('#LoginFormEmail').submit();
+    });
 });
