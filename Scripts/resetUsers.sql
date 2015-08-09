cfb0d4d366d037b4d941570d7002e2729f1645fe
Brad Baago <bbaago@gmail.com>
2015-08-09 19:52:55 -0400

    Added reset users script.

---
+++ Scripts/resetUsers.sql
@@ -0,0 +1,17 @@
+﻿DROP TABLE IF EXISTS 'CS_Users';
+
+CREATE TABLE 'CS_Users'
+(
+	id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	firstname VARCHAR(30) NOT NULL,
+	lastname VARCHAR(30) NOT NULL,
+	email VARCHAR(50), NOT NULL,
+	password VARCHAR(35) NOT NULL,
+	question VARCHAR(50), NOT NULL,
+	answer VARCHAR(35) NOT NULL,
+	quota BIGINT,
+	salt MEDIUMINT,
+	avatar BLOB,
+	active TINYINT,
+	reg_date TIMESTAMP,
+);
