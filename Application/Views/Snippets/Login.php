2ac8b8ed337c105eaf5a650c652b616245b7cd3a
Brad Baago <bbaago@gmail.com>
2015-08-09 16:41:03 -0400

    Improved usability.

--- Application/Views/Snippets/Login.php
+++ Application/Views/Snippets/Login.php
@@ -51,10 +51,10 @@
             <form action="index.php?c=home&m=updatePassword" method="post" name="LoginFormPassword" id="LoginFormPassword">
                 <div class="form-group" id='passwordGroup'>
                   <label for='Password'>Password</label>
-                  <div class="" id="passwordStrength"></div>
+                  <div class="" id="newpasswordStrength"></div>
                   <div class="form-inline">
-                    <input type="password" class="form-control" id="password1" name="password" placeholder="Password" style="width: 49%;">
-                    <input type="password" class="form-control" id="password2" placeholder="Password Confirm" style="width: 49%;">
+                    <input type="password" class="form-control" id="newpassword1" name="password" placeholder="Password" style="width: 49%;">
+                    <input type="password" class="form-control" id="newpassword2" placeholder="Password Confirm" style="width: 49%;">
                   </div>
                 </div>
                 <input type="hidden" id="email" name="email">
--- Web/JS/LoginForm.js
+++ Web/JS/LoginForm.js
@@ -1,4 +1,34 @@
 $(document).ready(function () {
+    $('#newpassword1, #newpassword2').on('keyup', function (e) {
+        if ($('#newpassword1').val() != '' && $('#newpassword2').val() != '' && $('#newpassword1').val() != $('#newpassword2').val()) {
+            $('#newpasswordStrength').removeClass().addClass('alert alert-danger').html('Passwords do not match!');
+            return false;
+        }
+        // Must have capital letter, numbers and lowercase letters
+        var strongRegex = new RegExp("^(?=.{8,})(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*\\W).*$", "g");
+
+        // Must have either capitals and lowercase letters or lowercase and numbers
+        var mediumRegex = new RegExp("^(?=.{7,})(((?=.*[A-Z])(?=.*[a-z]))|((?=.*[A-Z])(?=.*[0-9]))|((?=.*[a-z])(?=.*[0-9]))).*$", "g");
+
+        // Must be at least 6 characters long
+        var okRegex = new RegExp("(?=.{6,}).*", "g");
+
+        if (okRegex.test($(this).val()) === false) {
+            // If ok regex doesn't match the password
+            $('#newpasswordStrength').removeClass().addClass('alert alert-danger').html('Password must be 6 characters long.');
+            return false;
+        } else if (strongRegex.test($(this).val())) {
+            // If reg ex matches strong password
+            $('#newpasswordStrength').removeClass().addClass('alert alert-success').html('Good Password!');
+        } else if (mediumRegex.test($(this).val())) {
+            // If medium password matches the reg ex
+            $('#newpasswordStrength').removeClass().addClass('alert alert-info').html('Make your password stronger with more capital letters, more numbers and special characters!');
+        } else {
+            // If password is ok
+            $('#newpasswordStrength').removeClass().addClass('alert alert-danger').html('Weak Password, try using numbers and capital letters.');
+        }
+        return true;
+    });
     $("#SubmitLoginForm").click(function () {
         document.getElementById('LoginFormMessage').innerHTML = '';
         $("#LoginForm").submit(function (e) {
