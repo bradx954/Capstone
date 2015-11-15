ab1f3db5034bc4b99e20b7f8694fbd66602689d0
Brad Baago <brad.baago@linux.com>
2015-11-16 07:16:06 +0800

    Tweaked database scripts to allow for longer file and folder names.

--- Scripts/resetFiles.sql
+++ Scripts/resetFiles.sql
@@ -3,7 +3,7 @@
 CREATE TABLE CS_Files
 (
 	id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-	name VARCHAR(30),
+	name VARCHAR(100),
 	filesize BIGINT NOT NULL,
 	userid INT(6) NOT NULL,
 	folderid INT(6),
--- Scripts/resetFolders.sql
+++ Scripts/resetFolders.sql
@@ -3,7 +3,7 @@
 CREATE TABLE CS_Folders
 (
 	id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-	name VARCHAR(30),
+	name VARCHAR(100),
 	userid INT(6) NOT NULL,
 	folderid INT(6),
 	reg_date TIMESTAMP
